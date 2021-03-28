f2a6c119a349eb885263f723673838d90c82d677
Lauren Kessell <lkessell1@gmail.com>
2021-03-28 17:59:35 -0400

    Add remaining test solutions

--- src/librarian.js
+++ src/librarian.js
@@ -13,7 +13,18 @@ class Librarian {
   };
 
   findBook(title) {
-    return "Yes, we have " + title;
+    for (var i = 0; i <= this.library['shelves']['fantasy'].length; i++) {
+      if (this.library['shelves']['fantasy'][i].title === title) {
+        this.library['shelves']['fantasy'].pop();
+        return `Yes, we have ${title}`;
+      } else {
+        return `Sorry, we do not have ${title}`;
+      };
+    };
+  };
+
+  calculateLateFee(days){
+    return Math.ceil(days * 0.25);
   };
 };
 
