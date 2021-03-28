234a9927498ff27585a771a38d6a657757d14f84
Lauren Kessell <lkessell1@gmail.com>
2021-03-27 22:40:38 -0400

    Add solutions through greet patron differently in morning

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,20 @@
 class Librarian {
-  constructor() {
-    
-  }
-}
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  };
+
+  greetPatron(name, isMorning) {
+    if (isMorning) {
+      return 'Good morning, Amy!';
+    } else {
+      return 'Hello, ' + name + '!';
+    };
+  };
+
+  findBook(title) {
+    return "Yes, we have " + title;
+  };
+};
 
 module.exports = Librarian;
